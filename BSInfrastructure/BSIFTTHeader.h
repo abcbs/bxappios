c9498efc58d271b74c9390f4dbb4cfdfb00b18ad
LiuJQ9674Git <455298138@qq.com>
2015-07-08 01:54:02 +0800

    修改BLE包

---
+++ BSInfrastructure/BSIFTTHeader.h
@@ -0,0 +1,19 @@
+//
+//  BSIFTTHeader.h
+//  KTAPP
+//
+//  Created by admin on 15/7/8.
+//  Copyright (c) 2015年 itcast. All rights reserved.
+//
+
+#ifndef KTAPP_BSIFTTHeader_h
+#define KTAPP_BSIFTTHeader_h
+
+#import "BSBLEViewController.h"
+#import "LeTemperatureAlarmService.h"
+#import "LeDiscovery.h"
+
+#import "BSBLEDetailTableViewController.h"
+#import "BSBLEMainController.h"
+
+#endif
