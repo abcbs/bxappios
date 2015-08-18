ef3c6a310cd7eb94557ba61bf713334037ecb2c8
LiuJQ9674Git <455298138@qq.com>
2015-08-18 17:22:59 +0800

    本地通知方式实现

--- LBControl/MainFrame/AppDelegate.m
+++ LBControl/MainFrame/AppDelegate.m
@@ -73,6 +73,14 @@
     //kt.delegate=tabBardDelegate;
     //设置导航栏颜色
     //[BSUIComponentView navigationHeader:self.window.rootViewController.navigationController];
+    
+    //如果已经获得发送通知的授权则创建本地通知，否则请求授权(注意：如果不请求授权在设置中是没有对应的通知设置项的，也就是说如果从来没有发送过请求，即使通过设置也打不开消息允许设置)
+    if ([[UIApplication sharedApplication]currentUserNotificationSettings].types!=UIUserNotificationTypeNone) {
+        [self addLocalNotification];
+    }else{
+        [[UIApplication sharedApplication]registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeAlert|UIUserNotificationTypeBadge|UIUserNotificationTypeSound  categories:nil]];
+    }
+    
     return YES;
 }
 
@@ -86,10 +94,12 @@
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
 
+#pragma mark 进入前台后设置消息信息
+-(void)applicationWillEnterForeground:(UIApplication *)application{
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    [[UIApplication sharedApplication]setApplicationIconBadgeNumber:0];//进入前台取消应用消息图标
+}
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
@@ -106,6 +116,43 @@
     return UIInterfaceOrientationMaskPortrait;
 }
 
+#pragma mark 调用过用户注册通知方法之后执行（也就是调用完registerUserNotificationSettings:方法之后执行）
+-(void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings{
+    if (notificationSettings.types!=UIUserNotificationTypeNone) {
+        [self addLocalNotification];
+    }
+}
+
+#pragma mark - 私有方法
+#pragma mark 添加本地通知
+-(void)addLocalNotification{
+    
+    //定义本地通知对象
+    UILocalNotification *notification=[[UILocalNotification alloc]init];
+    //设置调用时间
+    notification.fireDate=[NSDate dateWithTimeIntervalSinceNow:10.0];//通知触发的时间，10s以后
+    notification.repeatInterval=2;//通知重复次数
+    //notification.repeatCalendar=[NSCalendar currentCalendar];//当前日历，使用前最好设置时区等信息以便能够自动同步时间
+    
+    //设置通知属性
+    notification.alertBody=@"最近添加了诸多有趣的特性，是否立即体验？"; //通知主体
+    notification.applicationIconBadgeNumber=1;//应用程序图标右上角显示的消息数
+    notification.alertAction=@"打开应用"; //待机界面的滑动动作提示
+    notification.alertLaunchImage=@"Default";//通过点击通知打开应用时的启动图片,这里使用程序启动图片
+    //notification.soundName=UILocalNotificationDefaultSoundName;//收到通知时播放的声音，默认消息声音
+    notification.soundName=@"msg.caf";//通知声音（需要真机才能听到声音）
+    
+    //设置用户信息
+    notification.userInfo=@{@"id":@1,@"user":@"Kenshin Cui"};//绑定到通知上的其他附加信息
+    
+    //调用通知
+    [[UIApplication sharedApplication] scheduleLocalNotification:notification];
+}
+
+#pragma mark 移除本地通知，在不需要此通知时记得移除
+-(void)removeNotification{
+    [[UIApplication sharedApplication] cancelAllLocalNotifications];
+}
 @end
 
 
